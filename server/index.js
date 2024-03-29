d0bac98c1313e9c238dd5cfdcf62da6c2929549c
VikasGgit <vikas.college001@gmail.com>
2024-03-29 12:36:56 +0530

    Socket io connected using cors and proxy

--- server/index.js
+++ server/index.js
@@ -4,17 +4,23 @@ const cors=require( "cors" );
 const socketIo=require( "socket.io" );
 
 const app = express();
+const server=http.createServer(app);
+const io=socketIo(server);
 const port=4500 || process.env.PORT;
 
+app.use(cors());
 app.get("/", (req, res)=>{
         res.send("ITS Working")
 })
+io.on("connection", (socket) => {
+    console.log("Client connected");
+    // socket.on("message", (data) => {
+    //     console.log("Message from client:", data);
+    //     // Handle the message or broadcast it to other clients
+    // });
+   
+});
 
-const server=http.createServer(app);
-const io=socketIo(server);
-io.on("connection", ()=>{
-    console.log("Connection established");
-})
 server.listen(port, ()=>{
         console.log("server listening on port: " + port);
 });
