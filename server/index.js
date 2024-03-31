9dc0ae002cd4fa1a9345b9775ff98ed2852487b1
VikasGgit <vikas.college001@gmail.com>
2024-03-31 10:16:10 +0530

    app is ready to deploy

--- server/index.js
+++ server/index.js
@@ -8,18 +8,35 @@ const server=http.createServer(app);
 const io=socketIo(server);
 const port=4500 || process.env.PORT;
 
+const users=[];
+
 app.use(cors());
+
 app.get("/", (req, res)=>{
         res.send("ITS Working")
 })
 io.on("connection", (socket) => {
-    console.log("Client connected");
-    // socket.on("message", (data) => {
-    //     console.log("Message from client:", data);
-    //     // Handle the message or broadcast it to other clients
-    // });
-   
+    
+
+    socket.on("joined", ({user}) =>{
+        console.log(users);
+        users[socket.id] = user;
+        // console.log(users)
+        console.log(`user : ${user}`);
+        socket.emit("welcome", {user: "Admin", message:`${users[socket.id]}, Welcome to Vikas Chat`});
+        socket.broadcast.emit("userJoined", {user: "Admin", message:`${users[socket.id]} has joined`});
+       
+    });
+    socket.on('disconnected',()=>{
+        socket.broadcast.emit("leave", {user: "Admin", message:`${users[socket.id]} has left`});
+        console.log(`${users[socket.id]} socket disconnected `);
+    })
+    socket.on("message", ({message, id})=>{
+        console.log(message, id);
+       io.emit("sendMessage",{user: users[id],message: message ,id: id});
+    });
 });
+
 
 server.listen(port, ()=>{
         console.log("server listening on port: " + port);
